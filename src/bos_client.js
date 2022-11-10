91fa6c8b54b9b68f45c4e7b07b549b57d04a51a9
meerkat <kit_hack@outlook.com>
2022-11-10 20:19:40 +0800

    feat: 增加文件夹分享接口，针对新域名设计新的鉴权算法

--- src/bos_client.js
+++ src/bos_client.js
@@ -1015,7 +1015,7 @@ BosClient.prototype.postObject = function (bucketName, key, data, options) {
 
 // --- E N D ---
 
-BosClient.prototype.sendRequest = function (httpMethod, varArgs) {
+BosClient.prototype.sendRequest = function (httpMethod, varArgs, requestUrl) {
     var defaultArgs = {
         bucketName: null,
         key: null,
@@ -1029,7 +1029,7 @@ BosClient.prototype.sendRequest = function (httpMethod, varArgs) {
     var args = u.extend(defaultArgs, varArgs);
 
     var config = u.extend({}, this.config, args.config);
-    var resource = path.normalize(path.join(
+    var resource = requestUrl || path.normalize(path.join(
         args.removeVersionPrefix ? '/' : '/v1',
         /\.[\w\-]+\.bcebos\.com$/.test(config.endpoint) ? '' : strings.normalize(args.bucketName || ''),
         strings.normalize(args.key || '', false)
@@ -1190,6 +1190,24 @@ BosClient.prototype._prepareObjectHeaders = function (options) {
     }
 
     return headers;
+};
+
+/**
+ *
+ * @param {{prefix:string,bucket:string,shareCode:string,durationSeconds:number}} body
+ * @returns
+ */
+BosClient.prototype.createFolderShareUrl = function (body, config) {
+    const endpoint = this.config.endpoint.replace(/^https?\:\/\//, '');
+    return this.sendRequest(
+        "POST",
+        {
+            body: JSON.stringify(u.extend({ endpoint: endpoint }, body)),
+            config: u.extend({ protocol: "https" }, config),
+            params: { action: 'urlGet' }
+        },
+        "https://bos-share.baidubce.com/"
+    );
 };
 
 module.exports = BosClient;
